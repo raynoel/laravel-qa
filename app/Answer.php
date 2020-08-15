13d49b35378856eecc46f9ff7ddb89ff89598e2c
ray <ray@ray.com>
2020-08-15 06:58:53 -0400

    Controles pour voter les réponses et installation de fontawsome

--- app/Answer.php
+++ app/Answer.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     
-    public function questions() {
+    public function question() {
         return $this->belongsTo(Question::class);                   // Une réponse appartient à une question
     }
 
@@ -24,5 +24,15 @@ class Answer extends Model
 
     public function getCreatedDateAttribute() {
         return $this->created_at->diffForHumans();                 // Converti 'created_at' en 'create_date' 
+    }
+
+    
+
+    public static function boot() {                                // Fonction exécutée lorsqu'on ajoute une rangée dans le tableau Answer
+        parent::boot();
+        static::created(function ($answer) {
+            // Obtient la question correspondant à 'quesion_id' de $answer et incrémente son champ 'answer_count'
+            $answer->question->increment('answers_count');  
+        });
     }
 }
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,25 +5,42 @@
   <div class="row justify-content-center">
     <div class="col-md-12">
       <div class="card">
-        <div class="card-header">
-          <!-- Entète -->
-          <div class="d-flex align-items-center">
-            <h2>{{ $question->title }}</h2>
-            <div class="ml-auto">
-              <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to All Questions</a>
-            </div>
-          </div>
-        </div>
-        <!-- carte avec une question -->
-        <div class="card-body">
-          {!! $question->body_html !!}
-          <div class="float-right">
-            <span class="text-muted">Answered {{ $question->created_date }}</span>
-            <div class="media mt-2">
-              <a href="{{ $question->user->url }}" class="pr-2"><img src="{{ $question->user->avatar }}"></a>
-              <div class="media-body mt-1">
-                <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+
+          <div class="card-body">
+            <div class="card-title">
+              <!-- Entète -->
+              <div class="d-flex align-items-center">
+                <h2>{{ $question->title }}</h2>
+                <div class="ml-auto">
+                  <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to All Questions</a>
+                </div>
               </div>
+            </div>
+
+            <hr>
+
+            <!-- carte avec une question -->
+            <div class="media">
+              <div class="d-fex flex-column vote-controls">
+                <a title="This question is useful" class="vote-up"><i class="fas fa-caret-up fa-3x"></i></a>
+                <span class="votes-count">1230</span>
+                <a title="This question is not useful" class="vote-down off"><i class="fas fa-caret-down fa-3x"></i></a>
+                <a title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                  <i class="fas fa-star fa-2x"></i>
+                  <span class="favorites-count">123</span>
+                </a>
+              </div>
+              <div class="media-body">
+                {!! $question->body_html !!}
+                <div class="float-right">
+                  <span class="text-muted">Answered {{ $question->created_date }}</span>
+                  <div class="media mt-2">
+                    <a href="{{ $question->user->url }}" class="pr-2"><img src="{{ $question->user->avatar }}"></a>
+                    <div class="media-body mt-1">
+                      <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                    </div>
+                  </div>
+                </div>
             </div>
           </div>
         </div>
@@ -38,31 +55,39 @@
           <div class="card-title">
             <h2>{{ $question->answers_count . " " . Str::plural('Answer', $question->answers_count) }}</h2>
           </div>
-          
+
           <hr>
 
           <!-- Affiche les réponses -->
           @foreach ($question->answers as $answer)
           <div class="media">
+            <div class="d-fex flex-column vote-controls">
+              <a title="This answer is useful" class="vote-up"><i class="fas fa-caret-up fa-3x"></i></a>
+              <span class="votes-count">1230</span>
+              <a title="This answer is not useful" class="vote-down off"><i class="fas fa-caret-down fa-3x"></i></a>
+              <a title="Mark this answer as best answer" class="vote-accepted mt-2"><i class="fas fa-check fa-2x"></i></a>
+            </div>
             <div class="media-body">
               {!! $answer->body_html !!}
-                <div class="float-right">
-                  <span class="text-muted">Answered {{ $answer->created_date }}</span>
-                  <div class="media mt-2">
-                    <a href="{{ $answer->user->url }}" class="pr-2"><img src="{{ $answer->user->avatar }}"></a>
-                    <div class="media-body mt-1">
-                      <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
-                    </div>
+              <div class="float-right">
+                <span class="text-muted">Answered {{ $answer->created_date }}</span>
+                <div class="media mt-2">
+                  <a href="{{ $answer->user->url }}" class="pr-2"><img src="{{ $answer->user->avatar }}"></a>
+                  <div class="media-body mt-1">
+                    <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
                   </div>
                 </div>
               </div>
             </div>
-            <hr>
-          @endforeach
+          </div>
 
-        </div>
+          <hr>
+
+      @endforeach
+
       </div>
     </div>
   </div>
+</div>
 </div>
 @endsection
